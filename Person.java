94102cf721676189580df3e126d710e0bc6fe494
RenzLayco <79893773+RenzLayco@users.noreply.github.com>
2021-03-06 22:17:09 +0800

    Update Person.java
    private person.java

--- Person.java
+++ Person.java
@@ -1,3 +1,27 @@
-public class Main {
-string fname = "renz";
-string lname = "layco";
+public int getlayco()  
+{  
+  return name;  
+}  
+public class name {  
+    private string name;  
+    public string getname() {  
+        return name;  
+    }  
+    public void setname(string newname) {  
+        name = name;  
+    }  
+}  
+
+
+public void set renz(string Age) {  
+    this.age = age;  
+}  
+public class name {  
+    private string name;  
+    public string getname() {  
+        return this.Age;  
+    }  
+    public void set name(string name) {  
+        this.name = name;  
+    }  
+}
